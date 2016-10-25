50fc663123ef2abf093086ce76794460216cc31a
Alpar Torok <alpar.torok@devfactory.com>
2016-10-25 16:31:44 +0300

    Set up webpack for the frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./js/app.jsx",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Joggr.io!',
+        inject: 'head',
+    })],
+    resolve: {
+        alias: {
+            joggr: path.resolve(__dirname, 'js')
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: '#source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel',
+          query: {
+            presets: [ 'react', 'es2015' ],
+            compact: true,
+            minified: true,
+            comments: false,
+            babelrc: false
+          }
+        }
+      ]
+    }
+}
+
