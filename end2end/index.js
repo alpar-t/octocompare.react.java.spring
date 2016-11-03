9551ad917a9183e06c60a542177eaee6941a836b
Alpar Torok <torokalpar@gmail.com>
2016-11-03 20:08:10 +0200

    Updated e2e to deal with auth and auto start backend

--- end2end/index.js
+++ end2end/index.js
@@ -1,105 +1,58 @@
-/*==============================================================================*/
-/* Casper generated Thu Nov 03 2016 11:28:21 GMT+0200 (EET) */
-/*==============================================================================*/
-
 var x = require('casper').selectXPath;
-casper.options.viewportSize = {width: 608, height: 1259};
+casper.options.viewportSize = {width: 1024, height: 720};
 casper.on('page.error', function(msg, trace) {
    this.echo('Error: ' + msg, 'ERROR');
    for(var i=0; i<trace.length; i++) {
        var step = trace[i];
        this.echo('   ' + step.file + ' (line ' + step.line + ')', 'ERROR');
    }
-   casper.exit(1);
+   this.capture('build/error.png')
+   this.exit(1);
+});
+casper.on("resource.error", function(resourceError){
+    this.echo('Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')', 'ERROR');
+    this.echo('Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString, 'ERROR');
+   this.exit(1);
 });
 
+casper.options.waitTimeout = 10000;
 
 //Capture all fails
 casper.test.on("fail", function () {
+    casper.capture('build/fail.png')
     casper.exit(1);
 });
 
-casper.test.begin('Resurrectio test', function(test) {
-   casper.start('http://localhost:8080/');
+casper.test.begin('Login test', function(test) {
+   casper.start(); 
+   casper.setHttpAuth('admin', 'password');
+   casper.thenOpen('http://127.0.0.1:8080/');
+   
    casper.waitForSelector("form input[name='username']",
        function success() {
+           casper.capture('build/loginForm.png');
            test.assertExists("form input[name='username']");
-           this.click("form input[name='username']");
+           casper.fill("form", {
+               "username": "admin",
+               "password": "password",
+           }, true)
        },
        function fail() {
+           this.capture("build/fail_input_form.png")
            test.assertExists("form input[name='username']");
    });
-   casper.waitForSelector("input[name='username']",
+
+   casper.waitWhileVisible("form input[name='username']");
+    
+   casper.waitUntilVisible(".fa.fa-bar-chart",
        function success() {
-           this.sendKeys("input[name='username']", "alpartralpar");
-       },
-       function fail() {
-           test.assertExists("input[name='username']");
-   });
-   casper.waitForSelector("body > div:nth-child(1) > div",
-       function success() {
-           test.assertExists("body > div:nth-child(1) > div");
-           this.click("body > div:nth-child(1) > div");
-       },
-       function fail() {
-           test.assertExists("body > div:nth-child(1) > div");
-   });
-   /* submit form */
-   casper.waitForSelector(".fa.fa-bar-chart",
-       function success() {
-           test.assertExists(".fa.fa-bar-chart");
+           casper.capture('build/after_login.png');
+           test.assertVisible(".fa.fa-bar-chart");
            this.click(".fa.fa-bar-chart");
        },
        function fail() {
-           test.assertExists(".fa.fa-bar-chart");
-   });
-   casper.waitForSelector(".fa.fa-bar-chart",
-       function success() {
-           test.assertExists(".fa.fa-bar-chart");
-           this.click(".fa.fa-bar-chart");
-       },
-       function fail() {
-           test.assertExists(".fa.fa-bar-chart");
-   });
-   casper.waitForSelector(".fa.fa-bar-chart",
-       function success() {
-           test.assertExists(".fa.fa-bar-chart");
-           this.click(".fa.fa-bar-chart");
-       },
-       function fail() {
-           test.assertExists(".fa.fa-bar-chart");
-   });
-   casper.waitForSelector(".fa.fa-plus",
-       function success() {
-           test.assertExists(".fa.fa-plus");
-           this.click(".fa.fa-plus");
-       },
-       function fail() {
-           test.assertExists(".fa.fa-plus");
-   });
-   casper.waitForSelector("form input[name='distanceMeters']",
-       function success() {
-           test.assertExists("form input[name='distanceMeters']");
-           this.click("form input[name='distanceMeters']");
-       },
-       function fail() {
-           test.assertExists("form input[name='distanceMeters']");
-   });
-   casper.waitForSelector("input[name='distanceMeters']",
-       function success() {
-           this.sendKeys("input[name='distanceMeters']", "3000");
-       },
-       function fail() {
-           test.assertExists("input[name='distanceMeters']");
-   });
-   casper.waitForSelector("input[name='timeSeconds']",
-       function success() {
-           this.sendKeys("input[name='timeSeconds']", "60");
-       },
-       function fail() {
-           test.assertExists("input[name='timeSeconds']");
+           test.assertVisible(".fa.fa-bar-chart");
    });
 
    casper.run(function() {test.done();});
-
 });
