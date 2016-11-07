36697f813a932afc058ecc42ec8d02f8c25c7bdf
Alpar Torok <torokalpar@gmail.com>
2016-11-07 08:18:34 +0200

    minor tweaks

--- backend/src/main/java/io/joggr/Application.java
+++ backend/src/main/java/io/joggr/Application.java
@@ -2,7 +2,6 @@ package io.joggr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
